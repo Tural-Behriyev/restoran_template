74956a9a918be8956e16fe5b514b273f9a2afcde
Tural Behriyev <turalbaxriyev@gmaile.com>
2022-11-22 10:09:02 +0400

    first commit

---
+++ JS/app.js
@@ -0,0 +1,18 @@
+// function myFunction() {
+//     var z = document.querySelector("#items");
+//     if (z.style.display == "block") {
+//       z.style.display = "none";
+//     } 
+//     else {
+//       z.style.display = "block";
+//     }
+//   }
+
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+}
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+}
+
+  
